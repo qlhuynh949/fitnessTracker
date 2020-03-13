07def118a9ee026d9bd83a419bcc6c3ee68b8f5f
Quan Huynh <qlhuynh@gmail.com>
2020-03-12 22:48:50 -0700

    add index.html

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
 
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 app.use(require('./routes'))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'))
+
+})
+
 
 require('./config')
   .then(() => app.listen(process.env.PORT || 3000))

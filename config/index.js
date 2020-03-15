10b8b2b951da41001d32fbadca070d660ce2cb16
Quan Huynh <qlhuynh@gmail.com>
2020-03-14 18:20:01 -0700

    adjustment for updates and add

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 
-module.exports = require('mongoose').connect('mongodb://heroku_r0kzf8fg:cc4815b2rhel1j0q6e2pel67pv@ds119692.mlab.com:19692/heroku_r0kzf8fg', {
+module.exports = require('mongoose').connect('mongodb://localhost/workout', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
